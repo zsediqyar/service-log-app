ab9315c343473a11606377992777d080e5adf44d
Zaki Sediqyar <zaki.sediqyar@gmail.com>
2018-09-25 05:27:12 +0000

    second

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
 var express         = require("express");
 var bodyParser      = require("body-parser");
 var methodOverride  = require("method-override");
-var mongodb         = require("mongodb");
+var mongo           = require("mongodb").MongoClient;
 var expHand         = require("express-handlebars");
 
 var app             = express();
 
 
+var url             = "mongodb://localhost:27017/exampleDb";
+    
+
+//********** LIBRARY SETUP
+app.engine('handlebars', expHand({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
-//**********LOGIN PAGE
-app.get("/", function(req, res) {
-   res.send("hi there") ;
+
+//********** LOGIN PAGE
+app.get("/", function(req, res, next) {
+   res.render("index");
+});
+
+
+//********** ALL RECORDS PAGE
+
+
+
+//********** ADD RECORD PAGE
+app.get("/records/new", function(req, res, next) {
+    res.render("new");
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//********** RECORD ADD PROCESS
 
 
 
---
+++ routes/routes.js
@@ -0,0 +1,36 @@
+// var express     = require("express");
+// var router      = express.Router();
+// var mongo     = require("mongodb");
+
+// var url         = "mongodb://localhost:27017/service_log";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// module.exports = router;
