7f0cc964da3bcceb28202185dc0d1f417383ac86
Za <zsediqyar@Zakis-MacBook-Pro.local>
2018-12-13 13:25:35 -0800

    cleaned mocha tests

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.exports = {
     sayHello: function () {
         return "Hello";
+    },
+    addNumbers: function (value1, value2) {
+        return value1 + value2;
     }
 }
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,15 +1,35 @@
-var assert = require("chai").assert;
-// var index  = require("../index");
-var sayHello = require("../index").sayHello;
+var assert          = require("chai").assert;
+var index           = require("../index");
+// var sayHello        = require("../index").sayHello;
+// var addNumbers      = require("../index").addNumbers;
+
+
+//RESULTS
+sayHelloResult = index.sayHello();
+addNumbersResult = index.addNumbers(5,5);
 
 describe("Index", function() {
-    it("sayHello should return hello", function() {
-        var result = sayHello();
-        assert.equal(result, "Hello")
+    describe("sayHello()", function () {
+        it("sayHello should return hello", function() {
+            // var result = index.sayHello();
+            assert.equal(sayHelloResult, "Hello")
+        });
+    
+        it("sayHello type should show string", function() {
+            // var result = index.sayHello();
+            assert.typeOf(sayHelloResult, "String");
+        });
     });
-
-    it("sayHello type should show string", function() {
-        var result = sayHello();
-        assert.typeOf(result, "String");
-    })
+    
+    describe("addNumbers()", function() {
+        it("addNumbers should be above 5", function() {
+            // var result = index.addNumbers(2,6);
+            assert.isAbove(addNumbersResult, 5);
+        });
+    
+        it("addNumbers type should be a number", function() {
+            // var result = index.addNumbers(5,5);
+            assert.typeOf(addNumbersResult, "number");
+        });
+    });
 });
